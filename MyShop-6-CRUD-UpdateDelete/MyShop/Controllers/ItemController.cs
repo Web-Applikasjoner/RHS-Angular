d73baead86b04d90febc29df70b908e8b038240e
KenanAbouShakra <89753703+Kenanshakra@users.noreply.github.com>
2023-10-23 04:21:50 +0200

    Add project files.

---
+++ MyShop-6-CRUD-UpdateDelete/MyShop/Controllers/ItemController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyShop.Models;
+using MyShop.ViewModels;
+
+namespace MyShop.Controllers;
+
+public class ItemController : Controller
+{
+    private readonly ItemDbContext _itemDbContext;
+
+    public ItemController(ItemDbContext itemDbContext)
+    {
+        _itemDbContext = itemDbContext;
+    }
+
+    public IActionResult Table()
+    {
+        List<Item> items = _itemDbContext.Items.ToList();
+        var itemListViewModel = new ItemListViewModel(items, "Table");
+        return View(itemListViewModel);
+    }
+
+    public IActionResult Grid()
+    {
+        List<Item> items = _itemDbContext.Items.ToList();
+        var itemListViewModel = new ItemListViewModel(items, "Grid");
+        return View(itemListViewModel);
+    }
+
+    public IActionResult Details(int id)
+    {
+        List<Item> items = _itemDbContext.Items.ToList();
+        var item = items.FirstOrDefault(i => i.ItemId == id);
+        if (item == null)
+            return NotFound();
+        return View(item);
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Item item)
+    {
+        if (ModelState.IsValid)
+        {
+            _itemDbContext.Items.Add(item);
+            _itemDbContext.SaveChanges();
+            return RedirectToAction(nameof(Table));
+        }
+        return View(item);
+    }
+
+    [HttpGet]
+    public IActionResult Update(int id)
+    {
+        var item = _itemDbContext.Items.Find(id);
+        if (item == null)
+        {
+            return NotFound();
+        }
+        return View(item);
+    }
+
+    [HttpPost]
+    public IActionResult Update(Item item)
+    {
+        if (ModelState.IsValid)
+        {
+            _itemDbContext.Items.Update(item);
+            _itemDbContext.SaveChanges();
+            return RedirectToAction(nameof(Table));
+        }
+        return View(item);
+    }
+
+    [HttpGet]
+    public IActionResult Delete(int id)
+    {
+        var item = _itemDbContext.Items.Find(id);
+        if (item == null)
+        {
+            return NotFound();
+        }
+        return View(item);
+    }
+
+    [HttpPost]
+    public IActionResult DeleteConfirmed(int id)
+    {
+        var item = _itemDbContext.Items.Find(id);
+        if (item == null)
+        {
+            return NotFound();
+        }
+        _itemDbContext.Items.Remove(item);
+        _itemDbContext.SaveChanges();
+        return RedirectToAction(nameof(Table));
+    }
+}
+    
---
+++ MyShop-8-Seeding/MyShop/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyShop.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+{
+    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+});
+
+builder.Services.AddDbContext<ItemDbContext>(options => {
+    options.UseSqlite(
+        builder.Configuration["ConnectionStrings:ItemDbContextConnection"]);
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    DBInit.Seed(app);
+}
+
+app.UseStaticFiles();
+
+app.MapDefaultControllerRoute();
+
+app.Run();
